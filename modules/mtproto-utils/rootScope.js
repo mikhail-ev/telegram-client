cda7dea56439b6ed0860e0c686fc9abc74745056
Dmitrii Uskov <>
2019-11-08 13:11:26 +0400

    console.war into $rootScope methods

--- modules/mtproto-utils/rootScope.js
+++ modules/mtproto-utils/rootScope.js
@@ -1,8 +1,10 @@
 window.$rootScope = (function () {
     return {
         $on: function () {
+            console.warn('$rootScope.$on was invoked')
         },
         $watch: function () {
+            console.warn('$rootScope.$watch was invoked')
         }
     }
 })();
