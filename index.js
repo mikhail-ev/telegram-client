f7900d443d88e31ebd19be6243aabc2189c897b4
Mikhail <targettaiga@gmail.com>
2019-11-08 11:17:33 +0100

    send code

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-window.MtpApiManager.invokeApi('help.getNearestDc', {}, {dcID: 2, createNetworker: true}).then(function (nearestDcResult) {
-    console.warn('>>>>>', nearestDcResult);
-    // if (wasCountry == $scope.credentials.phone_country) {
-    //     selectPhoneCountryByIso2(nearestDcResult.country)
-    // }
-    // if (nearestDcResult.nearest_dc != nearestDcResult.this_dc) {
-    //     MtpApiManager.getNetworker(nearestDcResult.nearest_dc, {createNetworker: true})
-    // }
-})
+window.MtpApiManager.invokeApi('help.getNearestDc', {}, {
+    dcID: 2,
+    createNetworker: true
+}).then(function (nearestDcResult) {
+    console.warn('1 >>>>>', nearestDcResult);
+    // auth.sendCode
+    window.MtpApiManager.invokeApi('auth.sendCode', {
+        flags: 0,
+        phone_number: '48730887261',
+        api_id: Config.App.id,
+        api_hash: Config.App.hash,
+        lang_code: navigator.language || 'en'
+    }, {
+        dcID: nearestDcResult.nearest_dc,
+        createNetworker: true
+    }).then(function (result) {
+        console.warn('2 >>>>>', result);
+    })
+});
